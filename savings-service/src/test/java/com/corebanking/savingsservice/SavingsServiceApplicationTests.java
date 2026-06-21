16e44448906c8f69ba6f6e54d50546ff40852684
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-06-21 21:33:37 +0100

    feat: create savings-service and configure savings-service application properties with separate database and port

---
+++ savings-service/src/test/java/com/corebanking/savingsservice/SavingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corebanking.savingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
