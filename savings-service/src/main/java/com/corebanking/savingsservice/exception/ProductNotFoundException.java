9183787d40a9a59940e39bdb99ae7a25ba64168e
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-06-22 10:44:04 +0100

    feat: add ProductNotFoundException.

---
+++ savings-service/src/main/java/com/corebanking/savingsservice/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.corebanking.savingsservice.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
