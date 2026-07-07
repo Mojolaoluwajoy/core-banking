db1d79df90d3e43dc69e2ba09bbd615a092b8b33
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-07 23:06:02 +0100

    feat: fixed-deposit-service content

---
+++ fixed-deposit-service/src/main/java/com/corebanking/fixeddepositservice/FixedDepositServiceApplication.java
@@ -0,0 +1,13 @@
+package com.corebanking.fixeddepositservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedDepositServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedDepositServiceApplication.class, args);
+	}
+
+}
