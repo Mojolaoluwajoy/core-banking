bc40f5c943181afb9bb1601008ba7e6557a4c0ac
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-08 09:58:48 +0100

    feat: add SwaggerConfig to configure OpenAPI documentation for fixed-deposit-service

---
+++ fixed-deposit-service/src/main/java/com/corebanking/fixeddepositservice/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+
+package com.corebanking.fixeddepositservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI fixedDepositServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fixed Deposit Service API")
+                        .description("Core Banking - Fixed Deposit Management Service")
+                        .version("1.0"));
+    }
+}
