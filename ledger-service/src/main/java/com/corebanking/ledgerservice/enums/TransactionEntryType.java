f2a5cedb10a7788518c116fab10e9718d227c150
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-08 13:22:02 +0100

    feat: add transaction entry type enums

---
+++ ledger-service/src/main/java/com/corebanking/ledgerservice/enums/TransactionEntryType.java
@@ -0,0 +1,8 @@
+package com.corebanking.ledgerservice.enums;
+
+public enum TransactionEntryType {
+    SAVINGS_DEPOSIT,
+    SAVINGS_WITHDRAWAL,
+    FIXED_DEPOSIT_CREATION,
+    FIXED_DEPOSIT_MATURITY
+}
