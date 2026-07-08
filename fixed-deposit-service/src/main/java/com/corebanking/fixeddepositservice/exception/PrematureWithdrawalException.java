cb74509a0a6404098f004e80cc101c724483bcfd
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-08 09:27:31 +0100

    feat: add PrematureWithdrawalException for early withdrawal attempts before maturity

---
+++ fixed-deposit-service/src/main/java/com/corebanking/fixeddepositservice/exception/PrematureWithdrawalException.java
@@ -0,0 +1,7 @@
+package com.corebanking.fixeddepositservice.exception;
+
+public class PrematureWithdrawalException extends RuntimeException {
+    public PrematureWithdrawalException(String message) {
+        super(message);
+    }
+}
