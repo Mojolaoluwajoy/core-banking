6c2ffaf2eed170b6f4170644f754d4cd6c9e103e
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-08 09:26:56 +0100

    feat: add FixedDepositNotFoundException for missing deposit lookups

---
+++ fixed-deposit-service/src/main/java/com/corebanking/fixeddepositservice/exception/FixedDepositNotFoundException.java
@@ -0,0 +1,7 @@
+package com.corebanking.fixeddepositservice.exception;
+
+public class FixedDepositNotFoundException extends RuntimeException {
+    public FixedDepositNotFoundException(String message) {
+        super(message);
+    }
+}
