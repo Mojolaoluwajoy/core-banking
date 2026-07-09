bd64b44902c5746a4b24fbc180c83d45f3a0e7cc
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-09 12:24:39 +0100

    feat: add LedgerEntryNotFoundException for missing ledger entry lookups

---
+++ ledger-service/src/main/java/com/corebanking/ledgerservice/exception/LedgerEntryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.corebanking.ledgerservice.exception;
+
+public class LedgerEntryNotFoundException extends RuntimeException {
+    public LedgerEntryNotFoundException(String message) {
+        super(message);
+    }
+}
