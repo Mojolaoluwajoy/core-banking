34420aa2b1a2d47b211ab1f474eb6e6c4b427452
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-13 16:30:37 +0100

    create fineract integration service and add the module to parent pom

---
+++ fineract-integration-service/src/main/java/com/corebanking/fineractintegrationservice/FineractIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.corebanking.fineractintegrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineractIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FineractIntegrationServiceApplication.class, args);
+	}
+
+}
