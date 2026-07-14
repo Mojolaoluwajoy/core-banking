07fc65bae3c31ba66230093eaecff70c0fc348dc
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-14 18:06:15 +0100

    feat: add FineractCreateClientRequest DTO mapping  request to Fineract's required format

---
+++ fineract-integration-service/src/main/java/com/corebanking/fineractintegration/dto/FineractCreateClientRequest.java
@@ -0,0 +1,20 @@
+
+package com.corebanking.fineractintegration.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FineractCreateClientRequest {
+
+    private Integer officeId;
+    private Integer legalFormId;
+    private String firstname;
+    private String lastname;
+    private boolean active;
+    private String activationDate;
+    private String dateFormat;
+    private String locale;
+    private String mobileNo;
+}
