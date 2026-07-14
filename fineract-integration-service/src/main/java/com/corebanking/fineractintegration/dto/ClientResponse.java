4b476a46e1ce9433e15b638ade4b3b023a5d2f33
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-14 18:16:05 +0100

    feat: add ClientResponse DTO for returning clean formatted client data to our callers

---
+++ fineract-integration-service/src/main/java/com/corebanking/fineractintegration/dto/ClientResponse.java
@@ -0,0 +1,15 @@
+
+package com.corebanking.fineractintegration.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ClientResponse {
+    private Long clientId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String status;
+}
