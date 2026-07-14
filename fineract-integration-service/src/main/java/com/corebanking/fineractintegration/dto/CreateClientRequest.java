43e6d5fed1faffd703b05fba60ce157d64204f43
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-14 18:05:33 +0100

    feat: add CreateClientRequest DTO defining our clean API format for client creation requests

---
+++ fineract-integration-service/src/main/java/com/corebanking/fineractintegration/dto/CreateClientRequest.java
@@ -0,0 +1,23 @@
+
+package com.corebanking.fineractintegration.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class CreateClientRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String phoneNumber;
+    private String dateOfBirth;
+}
