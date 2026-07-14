9e9cfdde9f3d0673c28cc24a8fa1a4753f5860b5
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-14 18:15:30 +0100

    feat: add FineractClientResponse DTO to capture Fineract's response after client creation

---
+++ fineract-integration-service/src/main/java/com/corebanking/fineractintegration/dto/FineractClientResponse.java
@@ -0,0 +1,17 @@
+/*
+ * FineractClientResponse
+ *
+ * WHAT: What Fineract returns after creating a client.
+ * WHY: We need to capture Fineract's response
+ * to extract the clientId and return it to our caller.
+ */
+package com.corebanking.fineractintegration.dto;
+
+import lombok.Data;
+
+@Data
+public class FineractClientResponse {
+    private Long clientId;
+    private Long officeId;
+    private Long resourceId;
+}
