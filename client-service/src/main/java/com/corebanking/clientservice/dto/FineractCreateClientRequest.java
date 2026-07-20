b56a489f67a0156375db2fd4458d9d9064b16419
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-20 17:39:01 +0100

    feat: add FineractCreateClientRequest DTO to client-service for Fineract API request format

---
+++ client-service/src/main/java/com/corebanking/clientservice/dto/FineractCreateClientRequest.java
@@ -0,0 +1,18 @@
+package com.corebanking.clientservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FineractCreateClientRequest {
+    private Integer officeId;
+    private Integer legalFormId;
+    private String firstname;
+    private String lastname;
+    private boolean active;
+    private String activationDate;
+    private String dateFormat;
+    private String locale;
+    private String mobileNo;
+}
