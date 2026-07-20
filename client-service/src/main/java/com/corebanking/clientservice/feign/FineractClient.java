63fd21802aa8471010bb3c5c3f2f5d28378fd029
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-20 17:48:51 +0100

    feat: add FineractClient Feign interface to client-service defining HTTP methods for Fineract client API endpoints

---
+++ client-service/src/main/java/com/corebanking/clientservice/feign/FineractClient.java
@@ -0,0 +1,22 @@
+
+package com.corebanking.clientservice.feign;
+
+import com.corebanking.clientservice.dto.FineractClientResponse;
+import com.corebanking.clientservice.dto.FineractCreateClientRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "fineract-client-service",
+        url = "${fineract.base-url}")
+public interface FineractClient {
+
+    @PostMapping("/clients")
+    FineractClientResponse createClient(
+            @RequestBody FineractCreateClientRequest request);
+
+    @GetMapping("/clients/{clientId}")
+    Object getClientById(@PathVariable("clientId") Long clientId);
+}
