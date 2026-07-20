d2c8d2a31ad7c0085de21ff8858d339ad15b3753
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-20 17:39:16 +0100

    feat: add FineractClientResponse DTO to client-service to capture Fineract client creation response

---
+++ client-service/src/main/java/com/corebanking/clientservice/dto/FineractClientResponse.java
@@ -0,0 +1,10 @@
+package com.corebanking.clientservice.dto;
+
+import lombok.Data;
+
+@Data
+public class FineractClientResponse {
+    private Long clientId;
+    private Long officeId;
+    private Long resourceId;
+}
